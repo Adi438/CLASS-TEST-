d548ce5043c545d17f0f9784cd0785ce385ea551
Adi438 <mishraaaditya54@gmail.com>
2022-05-21 22:19:44 +0530

    Anonymous Method

---
+++ CLASSTEST/Anonymous.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASSTEST
+{
+    internal class Anonymous
+    {
+        public class AnonymousMethods
+        {
+            public delegate string GreetingsDelegate(string name);
+            public static string Greetings(string name)
+            {
+                return "Hello @" + name + " Welcome to Anonymous Program";
+            }
+            static void Main(string[] args)
+            {
+                GreetingsDelegate gd = new GreetingsDelegate(AnonymousMethods.Greetings);
+                string GreetingsMessage = gd.Invoke("Aaditya");
+                Console.WriteLine(GreetingsMessage);
+                Console.ReadKey();
+            }
+        }
+    }
+}    
+                    
